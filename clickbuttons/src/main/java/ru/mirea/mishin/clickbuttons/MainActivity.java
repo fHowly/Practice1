3eeb459fbb9b4c8d80b5b34f8a1d9195af48a201
fHowly <night_fury5@mail.ru>
2022-03-24 14:54:30 +0300

    Init

---
+++ clickbuttons/src/main/java/ru/mirea/mishin/clickbuttons/MainActivity.java
@@ -0,0 +1,39 @@
+package ru.mirea.mishin.clickbuttons;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.TextView;
+import android.widget.Toast;
+
+public class MainActivity extends AppCompatActivity {
+    private TextView tvOut;
+    private Button buttonOk;
+    private Button buttonCancel;
+    public void onMyButtonClick(View view)
+    {
+        Toast.makeText(this,"Ещё один способ", Toast.LENGTH_SHORT).show();
+    }
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        tvOut = (TextView) findViewById(R.id.tvOut);
+        buttonOk = (Button) findViewById(R.id.btnOk);
+        buttonCancel = (Button) findViewById(R.id.btnCancel);
+
+        View.OnClickListener oclBtnOk = new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                tvOut.setText("Нажата кнопка ОК");
+            }
+        };
+
+        buttonOk.setOnClickListener(oclBtnOk);
+
+
+    }
+
+}
